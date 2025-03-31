b3a3ee7846747d98ad3e2360e858f87df4fa81f3
juanguiSO <preciadozapata@gmail.com>
2025-03-31 18:25:09 -0500

    Se agrego el cors

---
+++ src/main/java/com/agendamientos/agendamientosTurnos/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.agendamientos.agendamientosTurnos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica CORS a todos los endpoints
+                .allowedOrigins("http://localhost:5173") // Reemplaza con el origen de tu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Cabeceras permitidas
+                .allowCredentials(true); // Permite cookies y credenciales
+    }
+}
